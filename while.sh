7e9f74baa056c5b393b6b3efea40523379cdfa99
feliciapelaez <67351967+feliciapelaez@users.noreply.github.com>
2020-06-23 19:52:31 -0700

    LOL

---
+++ while.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+dateformatted=$(date +%m%d_%y-%H.%M.%S)
+
+echo "this is the date and time: " $date_formatted
+#!/bin/bash
+
+x = 1
+
+while
+
+    ((x<10))
+
+do
+    echo loop $x
+    date >date.$x
+    ((x=x+1))
+
+done
